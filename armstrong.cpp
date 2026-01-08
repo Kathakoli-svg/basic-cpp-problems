ac6c3060820a5b5342130be768874106c4807784
“Kathakoli <kathakolic@gmail.com>
2026-01-08 23:37:26 +0530

    Check palindrome

--- armstrong.cpp
+++ armstrong.cpp
@@ -4,18 +4,24 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+    int sum =0;
 	int dup = n;
     int revNum = 0;
-
-    while (n>=0) {
+	
+	while (n>0){
+		cnt = cnt + 1;
+		n = n / 10;
+	}
+    while (n>0) {
         int ld = n % 10;
         if (revNum > INT_MAX/10 || revNum < INT_MIN/10)
         	return 0;
+        sum = sum + pow;
         revNum = (revNum * 10) + ld;
         n = n / 10;
     }
 
-    if (dup=revNum)
+    if (dup=sum)
 		cout << "True";
 	else
 		cout << "False";
