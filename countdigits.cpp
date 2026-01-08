1f5f1f4c92613357a0bdfaa974d8f463a2810c20
“Kathakoli <kathakolic@gmail.com>
2026-01-08 13:08:01 +0530

    Initial commit of the program files

---
+++ countdigits.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int count(int n) {
+    int cnt = 0;
+
+    if (n == 0) return 1;   // edge case
+
+    while (n > 0) {
+        int lastDigit = n % 10; // not needed for counting, but kept as in image
+        cnt = cnt + 1;
+        n = n / 10;
+    }
+    return cnt;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << count(n);
+    return 0;
+}
