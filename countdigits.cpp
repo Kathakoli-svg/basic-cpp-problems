fac13c2c06d3608f6aeb0941c90bcc7ac5c84841
“Kathakoli <kathakolic@gmail.com>
2026-01-08 17:10:35 +0530

    Added a new code

--- countdigits.cpp
+++ countdigits.cpp
@@ -6,7 +6,7 @@ int count(int n) {
 
     if (n == 0) return 1;   // edge case
 
-    while (n > 0) {
+    while (n != 0) {
         int lastDigit = n % 10; // not needed for counting, but kept as in image
         cnt = cnt + 1;
         n = n / 10;
--- reversenum.cpp
+++ reversenum.cpp
@@ -7,8 +7,10 @@ int main() {
 
     int revNum = 0;
 
-    while (n > 0) {
+    while (n != 0) {
         int ld = n % 10;
+        if (revNum > INT_MAX/10 || revNum < INT_MIN/10)
+        	return 0;
         revNum = (revNum * 10) + ld;
         n = n / 10;
     }
