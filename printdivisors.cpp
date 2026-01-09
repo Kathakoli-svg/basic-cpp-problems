5716b8df970bd50ee8d65b1f15eb879167b469b7
“Kathakoli <kathakolic@gmail.com>
2026-01-09 13:56:52 +0530

    Print Divisors

---
+++ printdivisors.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printdivisors(int n){
+	vector<int> v;
+	
+	for(int i=1;i<=sqrt(n);i++){
+		if(n%i==0){
+			v.push_back(i);
+			if((n/i) != i){
+				v.push_back(n/i);
+			}
+		}
+	}
+	sort(v.begin(), v.end());
+	
+	for(auto it : v) 
+		cout << it << " ";
+}
+
+int main()
+{
+	int n;
+	cout << "The number: ";
+	cin >> n;
+	cout << "Divisors: ";
+	printdivisors(n);
+	return 0;
+}
